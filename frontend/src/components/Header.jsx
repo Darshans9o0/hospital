f5f5ad850345a6ef2ab660c1c1af6a90c2346c08
darshans9o0 <dats123>
2024-11-15 15:31:25 +0530

    new

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,26 +3,30 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex flex-col md:flex-row flex-wrap  bg-primary rounded-lg px-6 md:px-10 lg:px-20' >
         {/* left side */}
-        <div>
-            <p>
-                Book appointment <br /> with teusted doctor
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vh] md:mb-[-30px]'>
+            <p className='text-2xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight  lg:leading-tight whitespace-nowrap  '>
+            Book appointment <br />with trusted doctor
             </p>
-            <div>
-                <img src={assets.group_profiles} alt="" />
-                <p>
-                Simply browse through our extensive list of trusted doctors, <br />
-                schedule your appointment hassle-free. 
+            <div  className='flex flex-col  md:flex-row items-center gap-3 text-white font-light'>
+                <img className='w-28 ' src={assets.group_profiles} alt="" />
+                <p className='whitespace-wrap'>
+                Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block md:block' /> schedule your appointment hassle-free.
+                 
                 </p>
             </div>
-            <a href="">
-                Book Appointment <img src={assets.arrow_icon} alt="" />
+            <a href="#speciality" className='flex item center gap-2 bg-white px-8 py-3 rounded-full text-grey-600 text-sm m-auto mid:m-0 hover:scale-105 transition-all duration-300'>
+                Book Appointment <img  className='w-3' src={assets.arrow_icon} alt="" />
             </a>
 
         </div>
         {/* right  side */}
+        <div className='md:w-1/2 relative '>
+        <img className=' w-full md:absolute bottom-0 h-auto rounded-md' src={assets.header_img} alt="" />
 
+        </div>
+       
     </div>
   )
 }
---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SpecialityMenu = () => {
+  return (
+    <div id='speciality'>
+        <h1>Find by Specality</h1>
+
+
+    </div>
+  )
+}
+
+export default SpecialityMenu
